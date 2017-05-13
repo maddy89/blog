63f59b74a9b1e23bff6af44111be6ee569c01a42
madhawabandara <madawa911@gmail.com>
2017-05-14 00:49:59 +0530

    Refactoring

--- express/api-example/index.js
+++ express/api-example/index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
-// var router = require('./router.js');
 const MongoClient = require('mongodb').MongoClient
 var db;
-
-// app.use('/router', function(req, res, next){
-// 	console.log("A new request received at " + Date.now());
-// 	next(); //forwarding to next handler
-// });
 
 app.set('view engine', 'pug')
 app.set('views','./views');
@@ -32,9 +26,3 @@ MongoClient.connect('mongodb://localhost:27017/blog', (err, database) => {
     console.log('listening on 3000')
   })
 })
-
-// app.use('/router', router); //use router to handle requests comes to /router
-
-// app.listen(3000, function() {
-//     console.log("Listening on port 3000..");
-// });
